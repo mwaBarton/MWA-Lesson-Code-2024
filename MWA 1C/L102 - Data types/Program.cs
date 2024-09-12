d4e9a6fff98b374029bcd275f19168d22a72c276
mwa <mwa@barton.ac.uk>
2024-09-12 14:14:56 +0100

    L102

---
+++ MWA 1C/L102 - Data types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L102___Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = (int)4.5;
+
+            double number = 4;
+
+            Console.WriteLine(Math.Round(12.5));
+            Console.WriteLine(Math.Round(13.5, MidpointRounding.AwayFromZero));
+
+            Console.ReadKey();
+        }
+    }
+}
