550719a92508f7ae1739b9fede3e9cdb4064f0d3
mwa <mwa@barton.ac.uk>
2024-09-12 10:12:49 +0100

    L101

---
+++ MWA 1D/L101 - If/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L101___If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            if (name == "Matt")
+            {
+                Console.WriteLine("Welcome, back!");
+            } 
+            else if (name != "Aidan")
+            {
+                Console.WriteLine("You are not welcome here!");
+            }
+            else
+            {
+                Console.WriteLine("Welcome!");
+            }
+
+            Console.Write("Enter a number between 1 and 10: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0 && number <= 10)
+            {
+                Console.WriteLine("valid");
+            }
+            else if (number <= 0 || number > 10)
+            {
+                Console.WriteLine("invalid");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
