c40696fc1e6d9a0dcf2ffe399f064c7dee27ae1c
mwa <mwa@barton.ac.uk>
2024-10-14 16:00:14 +0100

    L114

---
+++ MWA 1C/L114 - Built-in functions/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L114___Built_in_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.ReadLine();
+            string s = "I'm bored of this";
+            //Console.WriteLine(Console.ReadLine());
+
+            Console.WriteLine(s[3]);
+
+            Console.WriteLine(s.Length);
+
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s);
+
+            char c = Console.ReadKey(true).KeyChar;
+
+            Console.WriteLine(c.ToString().ToUpper());
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("moved up");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("moved down");
+                    break;
+            }
+
+            string s1 = "matthew";
+
+            int index = s1.IndexOf("oo");
+
+            if (index == -1)
+            {
+                Console.WriteLine($"'oo' not found in {s1}");
+            } else
+            {
+                Console.WriteLine($"'oo' found at position {index}");
+            }
+            Console.WriteLine(index);
+
+            s1 = "matthew";
+            string s2 = "extension";
+
+            Console.WriteLine(string.Compare(s2, s2));
+
+            string s3 = "branch";
+            foreach (char letter in s3)
+            {
+                int code = (int)letter;
+                code = code - 32;
+                Console.Write((char)code);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1C/L114 - Built-in functions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L114 - Built-in functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril IT Services")]
+[assembly: AssemblyProduct("L114 - Built-in functions")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril IT Services 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2abaeba8-9930-42d2-8fdf-46d953b912ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
