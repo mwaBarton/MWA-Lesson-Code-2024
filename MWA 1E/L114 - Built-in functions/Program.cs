2249651742da83751d1807aa8f812330bb8f283c
mwa <mwa@barton.ac.uk>
2024-10-14 12:03:02 +0100

    L114

---
+++ MWA 1E/L114 - Built-in functions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L114___Built_in_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World";
+
+            Console.WriteLine(s.Length);
+
+            s.ToUpper();
+
+            string upper = s.ToUpper();
+            Console.WriteLine(upper);
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s);
+
+            char c = Console.ReadKey(true).KeyChar;
+            Console.WriteLine(c.ToString().ToUpper());
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("moved up");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("moved down");
+                    break;
+            }
+
+            string userWord = "morning";
+
+            int index = userWord.IndexOf("eve");
+
+            if (index != -1)
+            {
+                Console.WriteLine($"We found 'eve' at position {index}");
+            } else
+            {
+                Console.WriteLine("We could not find 'eve'");
+            }
+
+            string newString = "computer";
+
+            foreach (char letter in newString)
+            {
+                int code = (int)letter;
+                code = code - 32;
+                char newLetter = (char)code;
+
+                Console.WriteLine(newLetter);
+            }
+
+            // Comparing strings
+
+            string s1 = "Aadvark";
+            string s2 = "Able";
+
+            Console.WriteLine(string.Compare(s1, s2));
+            Console.WriteLine(string.Compare(s2, s1));
+            Console.WriteLine(string.Compare(s2, s2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1E/L114 - Built-in functions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L114 - Built-in functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril IT Services")]
+[assembly: AssemblyProduct("L114 - Built-in functions")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril IT Services 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("539ce847-60e2-4872-a006-c5a5fe5444b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
