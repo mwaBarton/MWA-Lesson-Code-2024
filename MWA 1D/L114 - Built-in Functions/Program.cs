49872060cd04aec85c568e74a6ab76416cd20646
mwa <mwa@barton.ac.uk>
2024-10-15 12:01:17 +0100

    L114

---
+++ MWA 1D/L114 - Built-in Functions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L114___Built_in_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadLine();
+
+            string s = "Hello World";
+
+            //Console.WriteLine(Console.ReadLine());
+
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s);
+
+            Console.WriteLine(s.Length);
+            //Console.WriteLine(Console.ReadLine().Length);
+
+            char c = Console.ReadKey(true).KeyChar;
+            if (c == 'a')
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("moved up");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("moved down");
+                    break;
+            }
+
+
+            string s1 = "bananananananananan";
+
+            Console.WriteLine(s1.IndexOf("nan"));
+
+            foreach (char letter in s1)
+            {
+                int code = (int)letter;
+                code = code - 42;
+                char newLetter = (char)code;
+                Console.Write(newLetter);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
