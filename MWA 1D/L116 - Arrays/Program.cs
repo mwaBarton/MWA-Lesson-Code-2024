d743ada7c70b7b69b9c5de3e63df81dee6ac2a4d
mwa <mwa@barton.ac.uk>
2024-10-18 16:02:01 +0100

    L116

---
+++ MWA 1D/L116 - Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace L116___Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] above18;
+
+            above18 = new bool[5];
+
+            above18[0] = true;
+            above18[1] = true;
+            above18[2] = false;
+            above18[3] = true;
+            above18[4] = true;
+
+            above18 = new bool[] { true, true, false, true, true };
+
+            for (int i = 0; i < above18.Length; i++)
+            {
+                Console.Write($"Enter above 18 for person {i}: ");
+                above18[i] = bool.Parse(Console.ReadLine());
+            }
+
+            foreach (bool a18 in above18) {
+                Console.Write(a18 + ",");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
