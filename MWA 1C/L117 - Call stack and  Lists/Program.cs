e2d78c85dd0b7a17e1f714777c531f7955ed25b2
mwa <mwa@barton.ac.uk>
2024-10-21 15:54:34 +0100

    L117 - call stack

---
+++ MWA 1C/L117 - Call stack and  Lists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L117___Call_stack_and__Lists
+{
+    internal class Program
+    {
+        static void SillySub(int num)
+        {
+            Console.WriteLine(num);
+            SillySub(num + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            SillySub(1);
+
+            Console.ReadKey();
+        }
+    }
+}
