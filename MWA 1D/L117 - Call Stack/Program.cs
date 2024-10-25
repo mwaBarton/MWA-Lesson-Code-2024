63f219dbd6bd2344671e2f29970dc06ac5b671fb
mwa <mwa@barton.ac.uk>
2024-10-25 13:16:08 +0100

    L117

---
+++ MWA 1D/L117 - Call Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L117___Call_Stack
+{
+    internal class Program
+    {
+
+        static void SillySub(int num)
+        {
+            Console.WriteLine(num);
+            SillySub(num + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            SillySub(1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1E/L117 - Call Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L117___Call_Stack
+{
+    internal class Program
+    {
+        static void SillySub(int number)
+        {
+            Console.WriteLine(number);
+            SillySub(number + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            SillySub(1);
+        }
+    }
+}
