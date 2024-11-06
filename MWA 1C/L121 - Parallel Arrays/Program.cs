4ca3b501087e9463cd2eec6e132c8a1be7384ccc
mwa <mwa@barton.ac.uk>
2024-11-06 10:14:35 +0000

    L121

---
+++ MWA 1C/L121 - Parallel Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L121___Parallel_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            Console.WriteLine("It was a dark and stormy night...");
+
+            System.Threading.Thread.Sleep(2500);
+
+            Console.WriteLine("...and the rain came down in torrents!");
+            Console.WriteLine("Press any key to continue...");
+
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+            Console.CursorVisible = true;
+            Console.ReadKey();
+
+            Console.Clear();
+
+            Console.WriteLine("Welcome to my game.");
+
+            Console.ReadKey();
+        }
+    }
+}
