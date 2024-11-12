c3df20c0de970e6f4a841bb07056c73bd6a659c9
mwa <mwa@barton.ac.uk>
2024-11-12 10:16:33 +0000

    L123

---
+++ MWA 1E/L124 - Reading Text Files/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L124___Reading_Text_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileContents = File.ReadAllText("colours.txt");
+            //Console.WriteLine(fileContents);
+            Console.ReadKey();
+
+            // The proper way
+            using (StreamReader sr = new StreamReader("colours.txt"))
+            {
+                while (sr.EndOfStream == false) { 
+                    string line = sr.ReadLine();
+                    if (line == "red") Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(line);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1E/L124 - Structures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L124___Structures
+{
+    internal class Program
+    {
+        struct Student
+        {
+            public int age;
+            public string name;
+            public bool isFirstYear;
+        }
+
+        static void Main(string[] args)
+        {
+            Student firstStudent;
+            Student secondStudent;
+
+            firstStudent.name = "My name David itself";
+            firstStudent.age = 16;
+            firstStudent.isFirstYear = true;
+
+            secondStudent.name = "Jacob";
+            secondStudent.age = 17;
+            secondStudent.isFirstYear = true;
+
+            Console.WriteLine($"{firstStudent.name}: Age={firstStudent.age}, Y1={firstStudent.isFirstYear}");
+            Console.WriteLine($"{secondStudent.name}: Age={secondStudent.age}, Y1={secondStudent.isFirstYear}");
+
+            Console.ReadKey();
+        }
+    }
+}
