4f76e9fc66772cfa0436a97a5bc1ff43595f9545
mwa <mwa@barton.ac.uk>
2024-11-12 11:59:27 +0000

    L123

---
+++ MWA 1D/L123 - Functions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace L123___Functions
+{
+    internal class Program
+    {
+        static string AlternatingCapitals(string phrase)
+        {
+            string result = "";
+            bool capital = false;
+            foreach (char letter in phrase)
+            {
+                if (capital)
+                {
+                    result += letter.ToString().ToUpper();
+                }
+                else
+                {
+                    result += letter.ToString().ToLower();
+                }
+
+                if ("qwertyuiopasdfghjklzxcvbnm".Contains(letter.ToString().ToLower()))
+                {
+                    capital = !capital; 
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter phrase: ");
+            string input = Console.ReadLine();
+            string result = AlternatingCapitals(input);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
