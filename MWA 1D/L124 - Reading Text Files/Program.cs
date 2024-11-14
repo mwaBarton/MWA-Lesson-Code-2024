f79aec672694718aed05ac984bee5e0a9a6cbf1c
mwa <mwa@barton.ac.uk>
2024-11-14 10:15:50 +0000

    L124

---
+++ MWA 1D/L124 - Reading Text Files/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L124___Reading_Text_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "colours.txt";
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while (sr.EndOfStream == false)
+                {
+                    string line = sr.ReadLine();
+                    if (line == "blue")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    //Console.WriteLine(line);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+
+            string contents = File.ReadAllText(filename);
+            Console.WriteLine(contents);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1D/L124 - Structures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L124___Structures
+{
+    internal class Program
+    {
+        struct Dog
+        {
+            public int aggression;
+            public int age;
+            public string breed;
+        }
+
+        static Dog TrainDog(Dog d)
+        {
+            Dog result;
+
+            result.aggression = d.aggression / 2;
+            result.age = d.age + 1;
+            result.breed = d.breed;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Dog dog;
+
+            dog.breed = "Labradoodle";
+            dog.age = -66;
+            dog.aggression = 100;
+
+            dog = TrainDog(dog);
+            Console.WriteLine(dog.aggression);
+
+            Console.ReadKey();
+        }
+    }
+}
