4f37ec3d0580c3c4ea91105bf73fc3a70586e12b
mwa <mwa@barton.ac.uk>
2024-11-21 09:21:39 +0000

    L126

---
+++ MWA 1D/L126 - Writing Text Files/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace L126___Writing_Text_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // The 'easy' way
+            File.WriteAllText("animal.noises", "moo\r\nmeow\r\noink\r\nwoof\r\nbaa\r\neeor\r\ngrr\r\n");
+
+
+            // The proper way
+            using (StreamWriter sw = new StreamWriter("animal.noises", true))
+            {
+                sw.WriteLine("rarr");
+                sw.WriteLine("bubble");
+
+                string line = " ";
+                while (line != "")
+                {
+                    Console.Write("Enter a new animal noise: ");
+                    line = Console.ReadLine();
+
+                    if (line != "")
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
