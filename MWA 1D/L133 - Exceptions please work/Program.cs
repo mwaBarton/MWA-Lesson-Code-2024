b115c9e040c91c211e2d005dcd07e7f4ecf21142
mwa <mwa@barton.ac.uk>
2024-12-12 09:47:55 +0000

    L133

---
+++ MWA 1D/L133 - Exceptions please work/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace L133___Exceptions_please_work
+{
+    internal class Program
+    {
+        static char GradeCalculator(int scorePercentage)
+        {
+            if (scorePercentage < 0 || scorePercentage > 100)
+            {
+                throw new ArgumentException("Score must be between 0 and 100.");
+            }
+
+            if (scorePercentage >= 80)
+            {
+                return 'A';
+            }
+            else if (scorePercentage >= 70)
+            {
+                return 'B';
+            }
+            else if (scorePercentage >= 60)
+            {
+                return 'C';
+            }
+            else if (scorePercentage >= 50)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'U';
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool success = false;
+            while (!success)
+            {
+                try
+                {
+                    int score;
+                    Console.Write("What's your percentage: ");
+                    score = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Your grade was " + GradeCalculator(score));
+                    success = true;
+                }
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: You must enter an integer");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: " + e.Message);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+
+            }
+
+            Console.WriteLine("bye.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1D/L133 - Exceptions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L133___Exceptions
+{
+    internal class Program
+    {
+        static char GradeCalculator(int scorePercentage)
+        {
+            if (scorePercentage >= 80)
+            {
+                return 'A';
+            }
+            else if (scorePercentage >= 70)
+            {
+                return 'B';
+            }
+            else if (scorePercentage >= 60)
+            {
+                return 'C';
+            }
+            else if (scorePercentage >= 50)
+            {
+                return 'D';
+            }
+            else if (scorePercentage >= 0)
+            {
+                return 'U';
+            }
+
+            return 'X';
+        }
+
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                int score;
+                Console.Write("What's your percentage: ");
+
+                score = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Your grade was " + GradeCalculator(score));
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("An error occurred.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            
+            Console.ReadKey();
+        }
+
+    }
+}
