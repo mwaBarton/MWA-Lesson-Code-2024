11b7906b22ea718fcbf228efa664758919026ced
mwa <mwa@barton.ac.uk>
2025-01-30 13:53:14 +0000

    L149

---
+++ MWA 1C/L149 - Classes and Objects/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Person
+    {
+        public string eyeColour;
+        public bool alive;
+        public int age;
+
+        public void Eat(string foodConsumed)
+        {
+            if (foodConsumed == "Poison")
+            {
+                if (alive)
+                {
+                    Console.WriteLine("They drank the poison and died.");
+                }
+                else
+                {
+                    Console.WriteLine("You poured poison on a dead person.");
+                }
+
+                alive = false;
+            } else if (alive)
+            {
+                Console.WriteLine(foodConsumed + " was eaten. It took 1 year to eat.");
+                age += 1;
+                Console.WriteLine("The person is now aged " + age);
+            }
+            else if (!alive) {
+                Console.WriteLine("Nothing happened because they are dead.");
+            }
+        }
+    }
+}
---
+++ MWA 1C/L149 - Classes and Objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person luke = new Person();
+            luke.age = 1;
+            luke.alive = true;
+            luke.eyeColour = "Brown";
+            luke.Eat("Pizza");
+
+            Person will = new Person();
+            will.age = 17;
+            will.alive = true;
+            will.eyeColour = "Grey";
+            will.Eat("Burger");
+            will.Eat("Poison");
+            will.Eat("Burger");
+
+            Console.ReadKey();
+        }
+    }
+}
