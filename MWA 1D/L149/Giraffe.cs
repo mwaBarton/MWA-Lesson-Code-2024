eb71d97bbc87e519af7df621940373215ed6621a
mwa <mwa@barton.ac.uk>
2025-01-31 15:48:00 +0000

    L149

---
+++ MWA 1D/L149/Giraffe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149
+{
+    internal class Giraffe
+    {
+        public float length, tongueLength;
+        public string nickname;
+        public int age;
+        public bool alive;
+
+        public void TongueFight(Giraffe opponent)
+        {
+            while (alive && opponent.alive)
+            {
+                opponent.length -= tongueLength * age;
+
+                length += opponent.tongueLength * opponent.age;
+
+                if (length <= 0)
+                {
+                    alive = false;
+                }
+
+                if (opponent.length <= 0)
+                {
+                    opponent.alive = false;
+                }
+                Console.WriteLine("lick.");
+            }
+
+            if (alive)
+            {
+                Console.WriteLine(nickname + " won and is now "+length+" long!");
+            } else
+            {
+                Console.WriteLine(opponent.nickname + " won and is now " + opponent.length + " long!");
+            }
+        }
+    }
+}
---
+++ MWA 1D/L149/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Giraffe geoffreyee = new Giraffe();
+            geoffreyee.alive = true;
+            geoffreyee.nickname = "geoff";
+            geoffreyee.age = 10;
+            geoffreyee.tongueLength = 50;
+            geoffreyee.length = 25;
+
+            Giraffe godfrey = new Giraffe();
+            godfrey.alive = true;
+            godfrey.nickname = "dead godfrey";
+            godfrey.age = -8;
+            godfrey.tongueLength = 0;
+            godfrey.length = 7006;
+
+            geoffreyee.TongueFight(godfrey);
+
+            Console.ReadKey();
+        }
+    }
+}
