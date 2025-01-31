eb9990244d1290aacee89836744c2dec612ea730
mwa <mwa@barton.ac.uk>
2025-01-31 14:04:48 +0000

    L149

---
+++ MWA 1E/L149 - Classes and Objects/Goose.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Goose
+    {
+        public float height, wingspan;
+        public bool canFloat;
+        public string favouriteBread;
+
+        public void Grow(int yearsElapsed)
+        {
+            if (favouriteBread == "German Rye bread")
+            {
+                height += yearsElapsed * 1000;
+                wingspan -= yearsElapsed;
+            }
+            else if (favouriteBread == "Poison bread")
+            {
+                canFloat = false;
+                Console.WriteLine("You fed the goose poison. Nice.");
+            }
+            else
+            {
+                height += yearsElapsed;
+                wingspan += yearsElapsed;
+            }
+
+            Console.WriteLine("The goose is now h="+height+" and w="+wingspan);
+        }
+    }
+}
---
+++ MWA 1E/L149 - Classes and Objects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Goose harold = new Goose();
+            harold.height = 100;
+            harold.wingspan = 30;
+            harold.canFloat = true;
+            harold.favouriteBread = "Tiger bread";
+
+            Goose ben = new Goose();
+            ben.height = 7000;
+            ben.wingspan = -4;
+            ben.canFloat = false;
+            ben.favouriteBread = "German Rye bread";
+
+            harold.Grow(5);
+            ben.Grow(2000);
+            ben.favouriteBread = "Poison bread";
+            ben.Grow(3);
+
+            Console.ReadKey();
+        }
+    }
+}
