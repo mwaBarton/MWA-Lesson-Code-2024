9228d26480e216d728892c577e76f3652309235a
mwa <mwa@barton.ac.uk>
2025-02-03 15:34:15 +0000

    L150

---
+++ MWA 1C/L150 - Access Modifiers and Constructors/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace L150___Access_Modifiers_and_Constructors
+{
+    internal class Car
+    {
+        public string colour;
+        private int price;
+        private bool priceSet = false;
+
+        public Car(int price)
+        {
+            this.price = price;
+        }
+
+
+        public void SetPrice(int p)
+        {
+            if (!priceSet)
+            {
+                if (p > 0)
+                {
+                    price = p;
+                    priceSet = true;
+                }
+            }
+        }
+
+        public void Drive(int distance)
+        {
+            
+            price = price - distance;
+            Console.WriteLine(price);
+        }
+    }
+}
---
+++ MWA 1C/L150 - Access Modifiers and Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L150___Access_Modifiers_and_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car(10);
+
+            mycar.SetPrice(-19);
+            mycar.SetPrice(10);
+            mycar.SetPrice(15);
+            mycar.colour = "Blue";
+            mycar.Drive(4);
+            
+            Console.ReadKey();
+        }
+    }
+}
