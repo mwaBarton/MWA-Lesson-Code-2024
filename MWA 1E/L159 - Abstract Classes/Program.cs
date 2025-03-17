343ab6b3eaf637a561a1cb870ebbc3c19782c425
mwa <mwa@barton.ac.uk>
2025-03-17 12:01:36 +0000

    L159

---
+++ MWA 1E/L159 - Abstract Classes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L159___Abstract_Classes
+{
+    public abstract class Consumable
+    {
+        protected bool consumed;
+        protected int power;
+        public Consumable(int inPower)
+        {
+            consumed = false;
+            power = inPower;
+        }
+        public abstract void Consume();
+        public bool IsConsumed() => consumed;
+    }
+
+    public class Potion : Consumable
+    {
+        public Potion(int inPower) : base(inPower) { }
+        public override void Consume()
+        {
+            consumed = true;
+        }
+    }
+
+    public class Snack : Consumable
+    {
+        protected int portionsLeft;
+        public Snack(int portions, int inPower) : base(inPower)
+        {
+            portionsLeft = portions;
+        }
+        public override void Consume()
+        {
+            portionsLeft--;
+            if (portionsLeft == 0) consumed = true;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Consumable> list = new List<Consumable>() { 
+                new Potion(50),
+                new Snack(2, 50)
+            };
+
+            bool done = false;
+            while (!done) {
+                done = true;
+                foreach (Consumable consumable in list)
+                {
+                    if (consumable.IsConsumed())
+                    {
+                        Console.WriteLine("already consumed");
+                    }
+                    else
+                    {
+                        consumable.Consume();
+                        done = false;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1E/L159 - Interfaces/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L159___Interfaces
+{
+    public interface ITasty
+    {
+        bool IsSalty();
+        void Eat();
+        void Season(List<string> seasoning);
+    }
+
+    public interface IConsumable
+    {
+        void Eat();
+        bool IsConsumed();
+    }
+
+    public class Hamburger : ITasty, IConsumable, IComparable<Hamburger>
+    {
+        private List<string> ingredients = new List<string>();
+        private bool consumed = false;
+
+        public int CompareTo(Hamburger other)
+        {
+            int salt = ingredients.Count(i => i == "salt");
+            int otherSalt = other.ingredients.Count(i => i == "salt");
+
+            if (salt > otherSalt) { return 1; }
+            else if (salt < otherSalt) { return -1; }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "[" + ingredients.Count(i => i == "salt") + " salts.]";
+        }
+
+        public void Eat()
+        {
+            consumed = true;
+            Console.WriteLine("yum. ,yum.");
+        }
+
+        public bool IsConsumed()
+        {
+            return consumed;
+        }
+
+        public bool IsSalty()
+        {
+            return ingredients.Contains("salt");
+        }
+
+        public void Season(List<string> seasoning)
+        {
+            ingredients.AddRange(seasoning);
+        }
+    }
+
+
+
+
+
+    internal class Program
+    {
+        static void EatFood(IConsumable food)
+        {
+            food.Eat();
+        }
+
+        static void AddManySaltsToFood(ITasty food, int numOfSalts)
+        {
+            List<string> salts = new List<string>();
+            for (int i = 0; i < numOfSalts; i++)
+            {
+                salts.Add("salt");
+            }
+            food.Season(salts);
+        }
+
+        static void Main(string[] args)
+        {
+            Hamburger food = new Hamburger();
+
+            AddManySaltsToFood(food, 400);
+            Hamburger ham2 = new Hamburger();
+            AddManySaltsToFood(ham2, 200);
+            Hamburger ham3 = new Hamburger();
+            AddManySaltsToFood(ham3, 300);
+            food.Eat();
+
+            List<Hamburger> hamburgerList = new List<Hamburger> { food, ham2, ham3 };
+            foreach (Hamburger h in hamburgerList)
+            {
+                Console.WriteLine(h);
+            }
+            Console.WriteLine("\n\n");
+            hamburgerList.Sort();
+
+            foreach (Hamburger h in hamburgerList)
+            {
+                Console.WriteLine(h);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
