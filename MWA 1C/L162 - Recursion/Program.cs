ceea5d917af3e5e17bab2f82462e41741299f949
mwa <mwa@barton.ac.uk>
2025-03-25 08:02:41 +0000

    Preparing for L161

---
+++ MWA 1C/L162 - Recursion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L162___Recursion
+{
+    internal class Program
+    {
+        static string[] results = new string[101];
+
+        static string Fun(int x)
+        {
+            if (results[x] != null)
+            {
+                //Console.Write(results[x]);
+                return results[x];
+            }
+            else if (x > 0)
+            {
+                string s = "";
+                s += Fun(x - 1);
+                //Console.Write(x);
+                s += x;
+                s += Fun(x - 1);
+                //results[x] = s;
+                //Console.WriteLine($"Saved x={x}, length={s.Length}");
+                return s;
+            }
+
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            Fun(28);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1E/L162 - Recursion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L162___Recursion
+{
+
+    internal class Program
+    {
+
+        static string[] results = new string[101];
+
+        static string f(int x) { 
+            if (results[x] != "bleh")
+            {
+                //Console.Write(results[x]);
+                return results[x];
+            }
+            else if (x > 0)
+            {
+                string s = "";
+                s += f(x - 1);
+                s += x;
+                //Console.Write(x);
+                s +=f(x - 1);
+                //results[x] = s;
+                return s;
+            }
+            else
+            {
+                return "";
+            }
+        } 
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = "bleh";
+            }
+
+            f(28);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
