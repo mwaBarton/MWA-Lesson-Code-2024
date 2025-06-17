ac7f331ad0966401d7e01a35090a9bcda39c95f2
mwa <mwa@barton.ac.uk>
2025-06-17 11:44:05 +0100

    FormsBitmapGraphics

---
+++ MWA 1D/FormsBitmapGraphics/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace FormsBitmapGraphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.display = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(12, 12);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(600, 400);
+            this.display.TabIndex = 0;
+            this.display.TabStop = false;
+            this.display.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 417);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.display)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox display;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MWA 1D/FormsBitmapGraphics/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsBitmapGraphics
+{
+    public partial class Form1 : Form
+    {
+        Bitmap B;
+        Graphics G;
+        Random r = new Random();
+        List<(int x, int y)> list = new List<(int x, int y)>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            B = new Bitmap(display.Width, display.Height);
+            G = Graphics.FromImage(B);
+
+
+            for (int x = 0; x < 1000; x++)
+            {
+                list.Add((r.Next(display.Width), r.Next(display.Height)));
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Update position
+
+            for (int x = 0; x < list.Count; x++)
+            {
+                list[x] = (list[x].x + r.Next(-3,4), list[x].y + r.Next(-5,6));
+
+            }
+
+            // Clear frame
+            G.Clear(Color.White);
+
+            // Draw
+            for (int x = 0; x < list.Count; x++)
+            {
+                G.FillEllipse(new SolidBrush(Color.Red), list[x].x, list[x].y, 10, 10);
+            }
+
+            // Update display
+            display.Image = B;
+        }
+    }
+}
